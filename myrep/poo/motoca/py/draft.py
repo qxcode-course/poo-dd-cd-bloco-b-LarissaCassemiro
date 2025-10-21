ff905780eaa161e11b5ddf78716ccd05d446eb6f
larissacassemiro2018@gmail.com <larissacassemiro2018@gmail.com>
2025-10-20 22:51:42 -0300

    grafite iniciada, mas não finalizada

--- myrep/poo/motoca/py/draft.py
+++ myrep/poo/motoca/py/draft.py
@@ -1,23 +1,3 @@
-# Este é um projeto de modelagem e implementação de uma motoca motorizada em um parque. A ideia é simular o funcionamento dessa motoca através de classes em um programa. Para isso, serão implementadas duas classes principais: `Pessoa` e `Moto`.
-
-# - Descrição
-#   - A classe `Moto` representa a motoca em si. Ela possui atributos como potência, tempo e a pessoa que está atualmente utilizando-a.
-#   - A motoca inicia com potência 1, sem minutos e sem ninguém.
-#   - Apenas uma pessoa pode estar na motoca por vez.
-#   - As funcionalidades principais da motoca incluem subir uma pessoa, descer uma pessoa, comprar tempo, dirigir por um tempo determinado e buzinar.
-#   - A classe `Pessoa` representa os usuários da motoca. Ela possui os atributos nome e idade.
-# - Comandos
-#   - Todos os comandos seguem o modelo `$comando arg1 arg2 ...`. Em caso de erro, uma mensagem adequada deve ser impressa.
-#   - `$show` - Mostra o estado atual da motoca, incluindo potência, tempo e pessoa atualmente na motoca.
-#     - `f"power:{this.power}, time:{this.time}, person:{this.person}"`
-#     - power:1, time:0, person:(marcos:4)
-#   - `$init` - Reinicia a motoca para o estado inicial, com potência 1, sem minutos e sem ninguém.
-#   - `$enter` - Permite uma pessoa subir na motoca. Deve ser seguido pelos argumentos `nome` e `idade` da pessoa.
-#   - `$leave` - Faz a pessoa atualmente na motoca descer.
-#   - `$buy` - Permite comprar tempo em minutos para utilizar a motoca. O tempo recebido é incrementado ao tempo atual.
-#   - `$drive` - Permite dirigir a motoca por um tempo determinado.
-#   - `$honk` - Permite buzinar a motoca.
-
 class Pessoa:
     def __init__(self, nome: str, idade: int):
         self.__nome = nome
